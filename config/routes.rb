a048580f61523b6a964a159af0b9944f8ba9eac2
MatthewSawatz <msawatzky2@rrc.ca>
2024-07-03 11:23:54 -0500

    Added nav bar pages for each model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root "books#index"
+  
+  get 'about', to: 'static_pages#about'
+  
   resources :books, only: [:index, :show]
   resources :authors, only: [:index, :show]
   resources :publishers, only: [:index, :show]
   resources :categories, only: [:index, :show]
   resources :reports, only: [:index]
-
 end
