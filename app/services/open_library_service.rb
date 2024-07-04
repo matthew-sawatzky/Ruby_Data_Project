f7380e8c4f5711b5923478c1c5c474fe310b8f24
MatthewSawatz <msawatzky2@rrc.ca>
2024-07-03 19:56:58 -0500

    added about page and show.html.erb links

--- app/services/open_library_service.rb
+++ app/services/open_library_service.rb
@@ -2,16 +2,16 @@ require 'net/http'
 require 'json'
 
 class OpenLibraryService
-  BASE_URL = "https://openlibrary.org/search.json?q=books"
+  BASE_URL = "https://openlibrary.org/search.json"
 
-  def fetch_books
-    uri = URI(BASE_URL)
+  def fetch_books(query)
+    uri = URI("#{BASE_URL}?q=#{query}")
     response = Net::HTTP.get(uri)
     JSON.parse(response)['docs']
   end
 
-  def import_books
-    fetch_books.each do |book_data|
+  def import_books(query)
+    fetch_books(query).each do |book_data|
       author = Author.find_or_create_by(name: book_data['author_name']&.first)
       publisher = Publisher.find_or_create_by(name: book_data['publisher']&.first)
       category = Category.find_or_create_by(name: book_data['subject']&.first)
@@ -27,4 +27,27 @@ class OpenLibraryService
       )
     end
   end
+
+  def import_fantasy_books
+    import_books('fantasy')
+  end
+
+  def import_scifi_books
+    import_books('science fiction')
+  end
+
+  def import_mystery_books
+    import_books('mystery')
+  end
+
+  def import_nonfiction_books
+    import_books('nonfiction')
+  end
+  def import_all_books
+    import_fantasy_books
+    import_scifi_books
+    import_mystery_books
+    import_nonfiction_books
+  end
 end
+
